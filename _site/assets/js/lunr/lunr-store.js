3dd9c9b9a47f3f4301b46e280d0e4f168373ce31
Ryan Levitt <rlevitt97@gmail.com>
2020-02-12 20:32:32 -0500

    overhaul to run website locally

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Data Wrangling Project",
+        "excerpt":"H1 HeadingH2 HeadingH3 HeadingHere’s some basic text.And here’s some italicsHere’s some bold text.What about a link?Here’s a bulleted list: First item Second item Third itemHere’s a numbered list: First Second ThirdPython code block: import numpy as np def test_function(x, y): z = np.sum(x,y) return zR code block:library(tidyverse)df &lt;- read_csv(\"some_file.csv\")head(df)Here’s some...","categories": [],
+        "tags": ["data wrangling","data science","messy data"],
+        "url": "http://localhost:4000/perceptron/",
+        "teaser":null}]
